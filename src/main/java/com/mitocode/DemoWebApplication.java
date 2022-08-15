cbbca3d4fc655c70e2e1ca8b7980a48f89e2bdcc
abc <karla@gmail.com>
2022-08-15 14:57:48 -0500

    mi primer practica

---
+++ src/main/java/com/mitocode/DemoWebApplication.java
@@ -0,0 +1,24 @@
+/**Primera Practica Spring boot
+ * Nombre:Karla Elizabeth
+ * Curso:Spring boot
+ * Numero practica:01
+ * Tema:Ver el funcionamiento al crear una pagina en spring boot
+ * Fecha:15/08/2022
+ * 
+ * **/
+
+
+package com.mitocode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoWebApplication.class, args);
+		
+	}
+
+}
